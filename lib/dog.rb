8889f170fcc9a730e22ca8bfc06d4d464921601a
Victoria Meng <nights1ststar@protonmail.com>
2018-05-02 03:29:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,12 +22,15 @@ class Dog
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO dogs (name, breed)
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, @name, @breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    self
+    if @id 
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, @name, @breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self
+    end
   end
 end
